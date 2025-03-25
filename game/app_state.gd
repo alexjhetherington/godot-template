00b21f51362f23e0dbf642eff2b25783d40be28a
Alex Hetherington <alexjhetherington@gmail.com>
2025-03-25 17:57:55 +0000

    Close settings menu when esc is pressed

--- game/app_state.gd
+++ game/app_state.gd
@@ -35,10 +35,13 @@ func _process(_delta):
 			pause(!is_paused)
 
 func pause(paused : bool):
-	is_paused = paused
-	get_tree().paused = is_paused
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	PauseMenu.enable(paused)
+	if (SettingsMenu.visible):
+		SettingsMenu._return()
+	else:
+		is_paused = paused
+		get_tree().paused = is_paused
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		PauseMenu.enable(paused)
 
 func on_console_opened():
 	console_open = true
